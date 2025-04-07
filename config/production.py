9bdb8b57f104f6961a302c1ce7fedfe177cac878
erimii <990303amy88@gmail.com>
2025-04-07 16:03:45 +0900

    dotenv 모듈을 사용하여 .env 파일에 저장한 정보를 os.getenv로 읽을수 있게 함

--- config/production.py
+++ config/production.py
@@ -7,6 +7,16 @@ Created on Mon Apr  7 10:09:15 2025
 
 from config.default import *
 
+from dotenv import load_dotenv
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'I\xe4\xe4^\xda\xcd\x88\xcaA\x08\xc5s\xaft\xed\x18'
+
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
